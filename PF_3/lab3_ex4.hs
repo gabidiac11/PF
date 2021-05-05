4d2d6d6d3909c95ee0e24b8c46fe2c175dd524f7
Gabriel Diac <gabi@thebrandwebbers.com>
2021-05-05 10:03:06 +0300

    lab3

---
+++ PF_3/lab3_ex4.hs
@@ -0,0 +1,40 @@
+{-
+    3.Arbori AVL
+
+    Scrieti o functie care calculeaza ınaltimea unui arbore:
+    height :: Arb -> Int
+
+    Scrieti o functie care testeaza daca un ABC este AVL:
+    isAVL :: Arb -> Bool
+
+    Scrieti functii care realizeaza rotatii simple la stanga/dreapta:
+    rotateLeft :: Arb -> Arb
+    rotateRight :: Arb -> Arb
+
+    Scrieti functii care realizeaza rotatii duble la stanga/dreapta:
+    doubleRotateLeft :: Arb -> Arb
+    doubleRotateRight :: Arb -> Arb
+
+    Scrieti o functie care reechilibreaza un nod:
+    echilibrare :: Arb -> Arb
+
+    Scrieti o functie care insereaza un element ıntr-un AVL (atentie la reechilibrare dupa inserarea propriu-zisa).
+    insertAVL :: Arb -> Integer -> Arb
+
+    Scrieti o functie care sterge un element dintr-un AVL (atentie la reechilibrare dupa stergerea
+    propriu-zisa).
+    removeAVL :: Arb -> Integer -> Arb
+    
+-}
+
+data Arb = Nod Integer Arb Arb 
+         | NodGol deriving (Show, Eq)
+
+
+
+heightArb :: Arb -> Integer
+heightArb NodGol = 0
+heightArb (Nod val NodGol NodGol) = 0
+heightArb (Nod val left right) = (+) 1 (max (heightArb left) (heightArb right))
+
+
